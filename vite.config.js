1ce3ef0a0688cda65203e37d1c7561b19fcf4dc8
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-20 20:28:44 +0200

    vite transfer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import svelte from '@sveltejs/vite-plugin-svelte';
+// import preprocess from 'svelte-preprocess';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+// export default {
+//  plugins: [svelte({ preprocess: preprocess() })],
+//  rollupdedupe: ['svelte']
+// };
+
+export default {
+  base: 'localhost'
+}
