cf757f1b948f32ab5ad2b58fd4df123ecd757023
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-20 14:50:24 +0200

    cesium-template-svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,10 @@
 	import { Viewer } from 'cesium';
 	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
 
+	if (typeof window !== "undefined"){
+		// browser code
+	}
+	
 	window.CESIUM_BASE_URL = './build';
 	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNTY0ZjMxYy1hZTdjLTRiMzQtYTc4Yi02NWQ5MzU4MWUxMjgiLCJpZCI6NDcwNzcsImlhdCI6MTYxNjg2MzYxOX0.V-4tUKhYM_XHdchqDu3MAAJPezusOzxMeimdYzCXd94';
    
