33cd50fb417d61be3fcd75753489f5c4449a1ab4
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-20 22:16:05 +0200

    vite, delete second index

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-// import svelte from '@sveltejs/vite-plugin-svelte';
-// import preprocess from 'svelte-preprocess';
-
-// export default {
-//  plugins: [svelte({ preprocess: preprocess() })],
-//  rollupdedupe: ['svelte']
-// };
-
 export default {
-  base: 'https://cloud-atlas.app'
+  base: 'localhost'
 }
