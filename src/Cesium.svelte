b356d88d150f95b97f6b3aa0fc78b8c4985eec21
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-23 17:33:20 +0200

    runs on dev browser but not on normal browser

--- src/Cesium.svelte
+++ src/Cesium.svelte
@@ -11,7 +11,7 @@
 		// browser code
 	}
 	
-	window.CESIUM_BASE_URL = './build';
+	window.CESIUM_BASE_URL = '/build/';
 	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNTY0ZjMxYy1hZTdjLTRiMzQtYTc4Yi02NWQ5MzU4MWUxMjgiLCJpZCI6NDcwNzcsImlhdCI6MTYxNjg2MzYxOX0.V-4tUKhYM_XHdchqDu3MAAJPezusOzxMeimdYzCXd94';
    
 	let viewer: Viewer;
