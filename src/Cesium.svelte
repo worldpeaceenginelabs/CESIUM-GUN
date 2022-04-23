25c3af2dc7a040375a0f5d7096c41a4cd3567a9d
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-23 17:44:10 +0200

    cesium base url

--- src/Cesium.svelte
+++ src/Cesium.svelte
@@ -7,13 +7,6 @@
 	import { Viewer } from 'cesium';
 	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
 
-	if (typeof window !== "undefined"){
-		// browser code
-	}
-	
-	window.CESIUM_BASE_URL = '/build/';
-	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNTY0ZjMxYy1hZTdjLTRiMzQtYTc4Yi02NWQ5MzU4MWUxMjgiLCJpZCI6NDcwNzcsImlhdCI6MTYxNjg2MzYxOX0.V-4tUKhYM_XHdchqDu3MAAJPezusOzxMeimdYzCXd94';
-   
 	let viewer: Viewer;
 	onMount(async () => {
 		viewer = new Viewer('cesiumContainer');
