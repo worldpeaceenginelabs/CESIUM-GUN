e66a863ef372f12541acb11c66770ef473002355
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-28 17:21:26 +0200

    added fly to user location on startup

--- src/Cesium.svelte
+++ src/Cesium.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
 
 	import { onMount } from 'svelte';
-	import * as Cesium from 'cesium';
-	import { Viewer, Cartesian2, Cartesian3 } from 'cesium';
-	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
-
-  import { addUserLocationInteraction } from './cesium_lib/addUserLocationInteraction'
 	
+  import {
+  Viewer, Cartesian3,
+  ClockRange,
+  Color,
+  Entity,
+  HermitePolynomialApproximation,
+  JulianDate,
+  SampledProperty,
+  } from 'cesium'
+
+  import * as Cesium from 'cesium';
+	import {  } from 'cesium';
+	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
+  
+
+
+  // Get user location from browser api
+  const getLocationFromNavigator = (): Promise<GeolocationPosition> => {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            resolve(position);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      } else {
+        reject(new Error('Geolocation is not supported by this browser.'));
+      }
+    });
+  };
+  
   // avoid "window not declared"
 
 	if (typeof window !== "undefined"){
@@ -14,12 +43,15 @@
 	}
 	
 
+
   // cesium viewer
   let viewer: Viewer;
 
+
+
   // cesium basic settings
 
-  onMount(async () => {
+  onMount(async () => { 
 		viewer = new Viewer('cesiumContainer', {
     "animation": false,
     "baseLayerPicker": true,
@@ -32,7 +64,12 @@
     "selectionIndicator": false,
     "timeline": false,
     "navigationHelpButton": false
-			
+
+    //Use OpenStreetMaps
+    // imageryProvider : new Cesium.OpenStreetMapImageryProvider({
+    //    url : 'https://a.tile.openstreetmap.org/'
+    // })
+    
 		});
 	});
 
@@ -40,53 +77,36 @@
 
 	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNTY0ZjMxYy1hZTdjLTRiMzQtYTc4Yi02NWQ5MzU4MWUxMjgiLCJpZCI6NDcwNzcsImlhdCI6MTYxNjg2MzYxOX0.V-4tUKhYM_XHdchqDu3MAAJPezusOzxMeimdYzCXd94';
   
-  // Get user location from browser api
-
-  const getLocationFromNavigator = (): Promise<GeolocationPosition> => {
-  return new Promise((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve(position);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-    } else {
-      reject(new Error('Geolocation is not supported by this browser.'));
-    }
-  });
-};
-
-// user location
-
+  // user location
 let userLocationCartesian: Cartesian3 | null;
 const userLocationPointId = 'user-location';
 
-
 // user location
-
   onMount(async (): Promise<void> => {
 let userLocation: GeolocationPosition | null = null;
-
 try {
 userLocation = await getLocationFromNavigator();
 }
-
 catch (error) {  
   // Ignore user decline
 }
 
-// user location
+// fly to user location on startup
 
+viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(userLocation.coords.longitude,
+                    userLocation.coords.latitude, 10000000.0),
+});
+
+
+// user location
 if (userLocation !== null) {
                 userLocationCartesian = Cartesian3.fromDegrees(
                     userLocation.coords.longitude,
                     userLocation.coords.latitude,
                 )
                 
-                addUserLocationInteraction(
+                addUserLocation(
                     viewer,
                     userLocation,
                     userLocationCartesian,
@@ -95,86 +115,62 @@ if (userLocation !== null) {
             }
 });
 
-
-// Globe Auto Rotate
-
-const addGlobeAutoRotation = (viewer: Viewer, rotationSpeed: number): void => {
-  viewer.canvas.addEventListener('mousemove', (event: MouseEvent) => {
-    const mousePosition = new Cartesian2(event.x, event.y);
-
-    // Get the position of the globe below the mouse position
-    // Will be undefined if the globe isn't below the cursor
-    const cartesian = viewer.camera.pickEllipsoid(
-      mousePosition,
-      viewer.scene.globe.ellipsoid
-    );
-
-    if (!cartesian) {
-      startRotation(viewer, rotationSpeed);
-      return;
-    }
-
-    stopRotation();
-  });
-
-  startRotation(viewer, rotationSpeed);
+const addUserLocation = (
+  viewer: Viewer,
+  userLocation: GeolocationPosition,
+  userLocationCartesian: Cartesian3,
+  userLocationPointId: string
+) => {
+  viewer.entities.add(
+    createPulsatingPoint(
+      viewer,
+      userLocationPointId,
+      Cartesian3.fromDegrees(
+        userLocation.coords.longitude,
+        userLocation.coords.latitude,
+        0
+      ),
+      Color.GREEN
+    )
+  );
 };
 
+// make user location dot pulsate
 
-// Globe Auto Rotate
-
-let rotationPaused = false;
-let lastRotationTime: number | null = null;
-let eventHandler: (() => void) | null = null;
-
-const doRotate = (viewer: Viewer, rotationSpeed: number) => {
-  if (rotationPaused) {
-    return;
-  }
-
-  const now = Date.now();
-  // Positiv: rotates from left to right
-  const spinRate = rotationSpeed;
-  const delta = (now - (lastRotationTime ?? now)) / 1000;
-
-  lastRotationTime = now;
-
-  viewer.scene.camera.rotate(Cartesian3.UNIT_Z, spinRate * delta);
-};
-
-const startRotation = (viewer: Viewer, rotationSpeed = 0.5) => {
-  // Already added, just continue the loop
-  if (eventHandler !== null) {
-    if (rotationPaused) {
-      // Updating this prevents a large rotation after a longer pause
-      lastRotationTime = Date.now();
-      rotationPaused = false;
-    }
-
-    return;
-  }
-
-  lastRotationTime = Date.now();
-  eventHandler = () => doRotate(viewer, rotationSpeed);
-
-  viewer.scene.postRender.addEventListener(eventHandler);
-};
-
-const stopRotation = () => {
-  rotationPaused = true;
-};
-
-
-
-
-
-
-
-
-
-
-
-
+let createPulsatingPoint = (
+    viewer: Viewer,
+    pointId: string,
+    userDestination: Cartesian3,
+    color: Color
+  ): Entity => {
+    const start = JulianDate.now();
+    const mid = JulianDate.addSeconds(start, 0.5, new JulianDate());
+    const stop = JulianDate.addSeconds(start, 2, new JulianDate());
+  
+    viewer.clock.startTime = start;
+    viewer.clock.currentTime = start;
+    viewer.clock.stopTime = stop;
+    viewer.clock.clockRange = ClockRange.LOOP_STOP;
+  
+    const pulseProperty = new SampledProperty(Number);
+    pulseProperty.setInterpolationOptions({
+      interpolationDegree: 3,
+      interpolationAlgorithm: HermitePolynomialApproximation,
+    });
+  
+    pulseProperty.addSample(start, 7.0);
+    pulseProperty.addSample(mid, 15.0);
+    pulseProperty.addSample(stop, 7.0);
+  
+    return new Entity({
+      id: pointId,
+      position: userDestination,
+      point: {
+        pixelSize: pulseProperty,
+        color,
+      },
+    });
+  };
 
 </script>
 
@@ -183,6 +179,10 @@ const stopRotation = () => {
 <main id="cesiumContainer">
 </main>
 
+<div class="ring-container">
+  <div class="ringring"></div>
+  <div class="circle"></div>
+</div> 
 
 
 <style>
@@ -192,5 +192,37 @@ main {
     width: 100%;
     height: 100%;
   }
+
+.ring-container {
+    position: relative;
+}
+  
+.circle {
+    width: 15px;
+    height: 15px;
+    background-color: #62bd19;
+    border-radius: 50%;
+    position: absolute;
+    top: 23px;
+    left: 23px;
+}
+
+.ringring {
+    border: 3px solid #62bd19;
+    border-radius: 30px;
+    height: 25px;
+    width: 25px;
+    position: absolute;
+    left: 15px;
+    top: 15px;
+    animation: pulsate 1s ease-out;
+    animation-iteration-count: infinite; 
+    opacity: 0.0
+}
+@keyframes pulsate {
+    0% {-webkit-transform: scale(0.1, 0.1); opacity: 0.0;}
+    50% {opacity: 1.0;}
+    100% {-webkit-transform: scale(1.2, 1.2); opacity: 0.0;}
+}
 
 </style>
