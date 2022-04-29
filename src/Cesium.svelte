0503b9f26d328cd9aae9ef4d9366b55afae0df48
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-29 17:15:55 +0200

    added globe rotation

--- src/Cesium.svelte
+++ src/Cesium.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	
   import {
   Viewer, Cartesian3,
@@ -100,12 +100,57 @@ catch (error) {
   // Ignore user decline
 }
 
+// Globe Auto Rotation
+
+let rotationPaused = false;
+let lastRotationTime: number | null = null;
+let eventHandler: (() => void) | null = null;
+
+const doRotate = (viewer: Viewer, rotationSpeed: number) => {
+  if (rotationPaused) {
+    return;
+  }
+
+  const now = Date.now();
+  // Positiv: rotates from left to right
+  const spinRate = rotationSpeed;
+  const delta = (now - (lastRotationTime ?? now)) / 1000;
+
+  lastRotationTime = now;
+
+  viewer.scene.camera.rotate(Cartesian3.UNIT_Z, spinRate * delta);
+};
+
+const startRotation = (viewer: Viewer, rotationSpeed = 0.5) => {
+  // Already added, just continue the loop
+  if (eventHandler !== null) {
+    if (rotationPaused) {
+      // Updating this prevents a large rotation after a longer pause
+      lastRotationTime = Date.now();
+      rotationPaused = false;
+    }
+
+    return;
+  }
+
+  lastRotationTime = Date.now();
+  eventHandler = () => doRotate(viewer, rotationSpeed);
+
+  viewer.scene.postRender.addEventListener(eventHandler);
+};
+
+const stopRotation = () => {
+  rotationPaused = true;
+};
+
+startRotation(viewer, -1);
+
 // fly to user location on startup
 
-viewer.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(userLocation.coords.longitude,
-                    userLocation.coords.latitude, 10000000.0),
-});
+// viewer.camera.flyTo({
+//    destination: Cesium.Cartesian3.fromDegrees(userLocation.coords.longitude,
+//                    userLocation.coords.latitude, 10000000.0),
+// });
 
 
 // user location
