17bc3020770bea1fb9da977cc13ab73181c53a23
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-29 22:48:16 +0200

    optimized padding and text align

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -4,14 +4,15 @@ import {fade} from 'svelte/transition'
 
 <div id="background"></div>
 <div id="modal">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/FKqOLmuIEFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<p><strong>
-    “You never change things by fighting against the existing reality.<br> To change something, build a new model that makes the old model obsolete.” <i>Buckminster Fuller</i><br><br>
-   COMING SOON<br><br>
+
+    <iframe width="475" height="345" src="https://www.youtube.com/embed/FKqOLmuIEFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    <p><strong>
+    “You never change things by fighting against the existing reality.<br><br> To change something, build a new model that makes the old model obsolete.”<br><br> <i>Buckminster Fuller</i><br><br><br>
+    COMING SOON<br><br>
     <a href="https://cloud-atlas.org" target="_blank">
-        Enter Projects Page</a>
-    </strong>
-</p>
+    Enter Projects Page</a>
+    </strong></p>    
 
 </div>
 
@@ -19,10 +20,6 @@ import {fade} from 'svelte/transition'
     #background {
         position: fixed;
         z-index: 1;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
     }
 
     #modal {
@@ -40,8 +37,10 @@ import {fade} from 'svelte/transition'
         background: #000;
         color: white;
         filter: drop-shadow(0 0 20px #333);
-        
+              
     }
+
+    p {padding: 0.5%;}
 
     a {text-decoration: none;
     color: grey;}
