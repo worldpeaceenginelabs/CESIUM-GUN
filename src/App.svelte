cfd481913ff27f03881ba30c444d025a777a141d
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-29 17:18:11 +0200

    cleaned teststuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-import { onMount } from "svelte";
+
 
   import Cesium from "./Cesium.svelte";
   import Modal from "./Modal.svelte";
-  import {fade} from 'svelte/transition'
- 
-  let isOpenModal = false;
-
-    function openModal() {
-        isOpenModal = true;
-    }
+  
 
   
 
