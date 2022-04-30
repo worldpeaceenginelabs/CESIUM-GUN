2cc37eb738774e190af0d4de5b351cdd046b7eff
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-30 16:13:23 +0200

    added modal fade in

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
-
-
+  
   import Cesium from "./Cesium.svelte";
   import Modal from "./Modal.svelte";
+  import {fade} from 'svelte/transition'
   
+  // 
+  let modalVisibility = false;
 
-  
+    function changeModalVisibility() {
+        modalVisibility = true;
+    }
 
 </script>
 
 
 
-<main>
-<Cesium/>
-<Modal/>
-</main>
+
+
+<div on:click={changeModalVisibility}>
+
+  <Cesium/>
+    
+    {#if modalVisibility}
+      <div transition:fade>
+      <Modal/>
+      </div>  
+    {/if}
+
+    </div>
 
 
 
 <style>
-
+ 
 </style>
