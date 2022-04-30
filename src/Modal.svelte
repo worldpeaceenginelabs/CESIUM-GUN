05ca1458fb779a81ee45760818d9e2cfea775347
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-04-30 16:14:11 +0200

    added responsiveness, button glitcheffect

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,47 +1,174 @@
 <script lang="ts">
-import {fade} from 'svelte/transition'
 </script>
 
-<div id="background"></div>
-<div id="modal">
 
-    <iframe width="475" height="345" src="https://www.youtube.com/embed/FKqOLmuIEFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    
-    <p><strong>
-    “You never change things by fighting against the existing reality.<br><br> To change something, build a new model that makes the old model obsolete.”<br><br> <i>Buckminster Fuller</i><br><br><br>
-    COMING SOON<br><br>
-    <a href="https://cloud-atlas.org" target="_blank">
-    Enter Projects Page</a>
-    </strong></p>    
+<div id="wrapper" >
+        
+    <div id="modal">
+        
+        <iframe src="https://www.youtube.com/embed/FKqOLmuIEFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        
+        <h4>
+        “You never change things by fighting against the existing reality.<br><br>
+        To change something, build a new model that makes the old model obsolete.”<br><br>
+        <i>Buckminster Fuller</i>
+        </h4>
+
+        <h2> COMING SOON<br></h2>
+   
+        <button>
+                
+                <a href="https://cloud-atlas.org" target="_blank">
+                    LEARN MORE</a>
+
+        </button>
+
+        <br>
+
+    </div>
 
 </div>
 
-<style>
-    #background {
+
+
+<style>    
+
+    #wrapper {
         position: fixed;
-        z-index: 1;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: center;  
     }
 
-    #modal {
+    #modal {                        
+        background: black;                
+		width: fit-content;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: center;
+        margin: 2%;  
+    }
+
+    iframe {
+        height: 300px;
+        width: 100%;       
+    }
+
+    h4 {
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-            sans-serif;
-          -webkit-font-smoothing: antialiased;
-          -moz-osx-font-smoothing: grayscale;
-        position: fixed;
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
         text-align: center;
-        z-index: 2;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background: #000;
         color: white;
-        filter: drop-shadow(0 0 20px #333);
-              
+        padding: 1%;        
     }
 
-    p {padding: 0.5%;}
+    h2 {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: white;
+        padding: 1%;        
+    }
 
-    a {text-decoration: none;
-    color: grey;}
+    a {
+        text-decoration: none;  
+    }
+
+    button, button::after {
+  width: 95%;
+  font-size: 25px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+  background: linear-gradient(45deg, transparent 5%, lightgrey 5%);
+  border: 0;
+  color: #fff;
+  letter-spacing: 3px;
+  line-height: 88px;
+  box-shadow: 6px 0px 0px darkslategrey;
+  outline: transparent;
+  position: relative;
+}
+
+button::after {
+  --slice-0: inset(50% 50% 50% 50%);
+  --slice-1: inset(80% -6px 0 0);
+  --slice-2: inset(50% -6px 30% 0);
+  --slice-3: inset(10% -6px 85% 0);
+  --slice-4: inset(40% -6px 43% 0);
+  --slice-5: inset(80% -6px 5% 0);
+  
+  content: 'AVAILABLE NOW';
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: linear-gradient(45deg, transparent 3%, #00E6F6 3%, #00E6F6 5%, #FF013C 5%);
+  text-shadow: -3px -3px 0px #F8F005, 3px 3px 0px #00E6F6;
+  clip-path: var(--slice-0);
+}
+
+button:hover::after {
+  animation: 1s glitch;
+  animation-timing-function: steps(2, end);
+}
+
+@keyframes glitch {
+  0% {
+    clip-path: var(--slice-1);
+    transform: translate(-20px, -10px);
+  }
+  10% {
+    clip-path: var(--slice-3);
+    transform: translate(10px, 10px);
+  }
+  20% {
+    clip-path: var(--slice-1);
+    transform: translate(-10px, 10px);
+  }
+  30% {
+    clip-path: var(--slice-3);
+    transform: translate(0px, 5px);
+  }
+  40% {
+    clip-path: var(--slice-2);
+    transform: translate(-5px, 0px);
+  }
+  50% {
+    clip-path: var(--slice-3);
+    transform: translate(5px, 0px);
+  }
+  60% {
+    clip-path: var(--slice-4);
+    transform: translate(5px, 10px);
+  }
+  70% {
+    clip-path: var(--slice-2);
+    transform: translate(-10px, 10px);
+  }
+  80% {
+    clip-path: var(--slice-5);
+    transform: translate(20px, -10px);
+  }
+  90% {
+    clip-path: var(--slice-1);
+    transform: translate(-10px, 0px);
+  }
+  100% {
+    clip-path: var(--slice-1);
+    transform: translate(0);
+  }
+}
+
 </style>
