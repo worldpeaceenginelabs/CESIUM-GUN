52418c960c417b9ee22acd6b6e64ed32cc083413
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-05-03 17:29:53 +0200

    Create SDGtopbarcss.svelte

---
+++ src/SDGtopbarcss.svelte
@@ -0,0 +1,30 @@
+<div class="square">
+    <div class="innersquare">
+    <div class="content">Test</div>
+</div>
+</div>
+
+
+<style>
+
+.square {
+    
+    background-color: black;
+    width: 50px;
+    height: 50px;
+}
+
+.innersquare {
+    
+    background-color: white;
+    width: 20px;
+    height: 20px;
+    
+}
+
+.content {
+    
+    color: #fff;
+}
+    
+</style>
