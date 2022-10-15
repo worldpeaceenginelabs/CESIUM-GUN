8273944bee00cf9cd4038bab4ed1380938d28430
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-10-15 14:53:58 +0200

    Cesium-Gun clean

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
   
 import Cesium from "./Cesium.svelte"
-import ComingSoon from "./Comingsoontext.svelte"
-import Modal from "./Modalmain.svelte"
-import Videoquotetextlink from "./Videoquotetextlink.svelte";
-import {fade} from 'svelte/transition'
-import Sdgtopbar from "./Sdgtopbar.svelte";
-import Publicorprivate from "./Publicorprivate.svelte";
-
   
 	// initialise modal state and content
 	let showModal = false;
@@ -22,25 +15,10 @@ import Publicorprivate from "./Publicorprivate.svelte";
 </script>
 
 <!-- Here starts the app-->
-<!-- on:click={() => (toggleModal(Videoquotetextlink))}  -->
 <div>
-
-  <Cesium/>
-  <Sdgtopbar/>
-  <!-- <ComingSoon/> -->
-  
+<Cesium/>
 </div>
 
-{#if showModal}
-  <div transition:fade>
-  <Modal on:click={toggleModal} {modalContent}/>
-  </div>
-  {/if}
 
-
-
-
-<style>
-  
- </style>
+<style></style>
 
