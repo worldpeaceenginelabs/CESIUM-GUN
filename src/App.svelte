078fec20950af04b7d6e709b9d3421161cbf8207
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-10-15 17:23:43 +0200

    removed modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,11 @@
-<script lang="ts">
-  
+<script lang="ts">  
 import Cesium from "./Cesium.svelte"
-  
-	// initialise modal state and content
-	let showModal = false;
-	let modalContent;
-	
-	// pass in component as parameter and toggle modal state
-	function toggleModal(component) {
-		modalContent = component;
-		showModal = !showModal;
-	}
-
 </script>
 
 <!-- Here starts the app-->
 <div>
 <Cesium/>
 </div>
-
 
 <style></style>
 
